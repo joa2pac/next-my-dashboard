7c4816c0f80a98243f230e55bb72bb52915b7d20
joaquin <joaquinahuel93@gmail.com>
2024-07-26 19:58:22 -0300

    update

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,15 @@
+import { CartCounter } from "@/app/shopping-cart";
+
+export const metadata = {
+  title: "Shopping Cart",
+  description: "Simple contador",
+};
+
 export default function CounterPage() {
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CartCounter />
     </div>
   );
 }
