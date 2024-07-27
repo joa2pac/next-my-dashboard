8bd8d91663804dd588286e01b6279585977c6af7
joaquin <joaquinahuel93@gmail.com>
2024-07-27 00:59:06 -0300

    update

--- src/app/dashboard/pokemon/page.tsx
+++ src/app/dashboard/pokemon/page.tsx
@@ -1,7 +1,13 @@
-export default function PokemonsPage() {
-  return (
-    <div>
-      <h1>Hello Pokemon Page</h1>
-    </div>
-  );
+const getPokemons = async (limit = 20, offset = 0) => {
+  const data = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then((res) => res.json());
+
+  return data;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons();
+
+  return <div>{JSON.stringify(pokemons)}</div>;
 }
