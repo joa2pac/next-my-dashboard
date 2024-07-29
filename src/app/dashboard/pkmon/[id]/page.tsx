9c9f926d346dce15a7754a7e4681f181d4113380
joaquin <joaquinahuel93@gmail.com>
2024-07-29 17:30:22 -0300

    update

--- src/app/dashboard/pkmon/[id]/page.tsx
+++ src/app/dashboard/pkmon/[id]/page.tsx
@@ -1,11 +1,25 @@
+import { Tokemon } from "@/app/pokemons/interfaces/pokemon";
+
 interface Props {
   params: { id: string };
 }
 
-export default function PkmonPage({ params }: Props) {
+const getPokemon = async (id: string): Promise<Tokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: "force-cache",
+  }).then((resp) => resp.json());
+
+  console.log("pokemon name:", pokemon.name);
+
+  return pokemon;
+};
+
+export default async function PkmonPage({ params }: Props) {
+  const pokemon = await getPokemon(params.id);
+
   return (
     <div>
-      <h1>Pókemon {params.id}</h1>
+      <h1>{JSON.stringify(pokemon)}</h1>
     </div>
   );
 }
--- src/app/pokemons/index.ts
+++ src/app/pokemons/index.ts
@@ -2,3 +2,4 @@ export { PokemonCard } from "./components/PokemonCard";
 export { PokemonGrid } from "./components/PokemonGrid";
 export type { PokemonsResponse } from "./interfaces/pokemons-response";
 export type { SimplePokemon } from "./interfaces/simple-pokemon";
+export type { Tokemon } from "./interfaces/pokemon";
