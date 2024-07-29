6e69021141530846c0f8b3f73a6a435a08b5380b
joaquin <joaquinahuel93@gmail.com>
2024-07-29 20:04:27 -0300

    update

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,45 +1,61 @@
+import { Pokemon } from "@/pokemons";
 import { Metadata } from "next";
-import Image from "next/image";
-
-import { Tokemon } from "@/app/pokemons/interfaces/pokemon";
+import Image from 'next/image';
 import { notFound } from "next/navigation";
+
 
 interface Props {
   params: { id: string };
 }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+
+export async function generateMetadata({ params }:Props): Promise<Metadata> {
+
   try {
     const { id, name } = await getPokemon(params.id);
-
+  
     return {
-      title: `#${id} - ${name}`,
-      description: `Pagina del Pokemon ${name}`,
-    };
+      title: `#${ id } - ${ name }`,
+      description: `Página del pokémon ${ name }`
+    }
+    
   } catch (error) {
     return {
-      title: "Pagina del pokemon",
-      description: "Atrapalos sha",
-    };
+      title: 'Página del pokémon',
+      description: 'Culpa cupidatat ipsum magna reprehenderit ex tempor sint ad minim reprehenderit consequat sit.'
+    }
   }
 }
 
-const getPokemon = async (id: string): Promise<Tokemon> => {
+
+const getPokemon = async(id: string): Promise<Pokemon> => {
+
+
   try {
-    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
-      cache: "force-cache",
-    }).then((resp) => resp.json());
-
-    console.log("pokemon name:", pokemon.name);
-
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${ id }`,{
+      cache: 'force-cache'// TODO: cambiar esto en un futuro
+      // next: {
+      //   revalidate: 60 * 60 * 30 * 6
+      // }
+    }).then( resp => resp.json() );
+  
+    console.log('Se cargó: ', pokemon.name);
+  
     return pokemon;
+    
   } catch (error) {
-    return notFound();
+    notFound();
   }
-};
+
+}
+
+
+
 
 export default async function PokemonPage({ params }: Props) {
+
   const pokemon = await getPokemon(params.id);
+
 
   return (
     <div className="flex mt-5 flex-col items-center text-slate-800">
@@ -50,42 +66,49 @@ export default async function PokemonPage({ params }: Props) {
           </h1>
           <div className="flex flex-col justify-center items-center">
             <Image
-              src={pokemon.sprites.other?.dream_world.front_default ?? ""}
+              src={pokemon.sprites.other?.dream_world.front_default ?? ''}
               width={150}
               height={150}
               alt={`Imagen del pokemon ${pokemon.name}`}
               className="mb-5"
             />
 
+
             <div className="flex flex-wrap">
-              {pokemon.moves.map((move) => (
-                <p key={move.move.name} className="mr-2 capitalize">
-                  {move.move.name}
-                </p>
-              ))}
+              {
+                pokemon.moves.map(move => (
+                  <p key={move.move.name} className="mr-2 capitalize">{move.move.name}</p>
+                ))
+              }
             </div>
           </div>
         </div>
         <div className="grid grid-cols-2 gap-4 px-2 w-full">
+
           <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg ">
             <p className="text-sm text-gray-600">Types</p>
             <div className="text-base font-medium text-navy-700 flex">
-              {pokemon.types.map((type) => (
-                <p key={type.slot} className="mr-2 capitalize">
-                  {type.type.name}
-                </p>
-              ))}
+              {
+                pokemon.types.map(type => (
+                  <p key={type.slot} className="mr-2 capitalize">{type.type.name}</p>
+                ))
+              }
             </div>
           </div>
 
           <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg ">
             <p className="text-sm text-gray-600">Peso</p>
-            <span className="text-base font-medium text-navy-700 flex">{pokemon.weight}</span>
+            <span className="text-base font-medium text-navy-700 flex">
+              {
+                pokemon.weight
+              }
+            </span>
           </div>
 
           <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg">
             <p className="text-sm text-gray-600">Regular Sprites</p>
             <div className="flex justify-center">
+
               <Image
                 src={pokemon.sprites.front_default}
                 width={100}
@@ -99,12 +122,14 @@ export default async function PokemonPage({ params }: Props) {
                 height={100}
                 alt={`sprite ${pokemon.name}`}
               />
+
             </div>
           </div>
 
           <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg">
             <p className="text-sm text-gray-600">Shiny Sprites</p>
             <div className="flex justify-center">
+
               <Image
                 src={pokemon.sprites.front_shiny}
                 width={100}
@@ -118,8 +143,12 @@ export default async function PokemonPage({ params }: Props) {
                 height={100}
                 alt={`sprite ${pokemon.name}`}
               />
+
             </div>
           </div>
+
+
+
         </div>
       </div>
     </div>
--- src/app/pokemons/index.ts
+++
@@ -1,5 +0,0 @@
-export { PokemonCard } from "./components/PokemonCard";
-export { PokemonGrid } from "./components/PokemonGrid";
-export type { PokemonsResponse } from "./interfaces/pokemons-response";
-export type { SimplePokemon } from "./interfaces/simple-pokemon";
-export type { Tokemon } from "./interfaces/pokemon";
