9bcd0d44b16bad5f37d70e1b325825a49629d39d
joaquin <joaquinahuel93@gmail.com>
2024-08-03 01:07:24 -0300

    update

--- src/app/api/counter/route.ts
+++ src/app/api/counter/route.ts
@@ -2,6 +2,14 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   return NextResponse.json({
+    method: "GET EXECUTED",
+    count: 100,
+  });
+}
+
+export async function POST(request: Request) {
+  return NextResponse.json({
+    method: "POST EXECUTED",
     count: 100,
   });
 }
